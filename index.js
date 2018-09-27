023a9d2bf647b93d6f0f976308b97eac0b04e9d1
Sarah Shin <sarahsujin.shin@gmail.com>
2018-09-27 23:54:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-function takeANumber(currentLine,newPersonName){
-  
+function takeANumber(katzDeliLine,name){
+  katzDeliLine.push(name);
+  return(`Welcome, ${name}. You are number ${katzDeliLine.indexOf(name)+1} in line.`);
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return 'There is nobody waiting to be served!';
+  }
+  else {
+    return katzDeliLine[0];
+    katzDeliLine.slice[0];
+  }
+}
+
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return 'The line is currently empty.';
+  }
+  else {
+    
+  }
 }
