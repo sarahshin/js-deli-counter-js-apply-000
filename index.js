30d23c7b3ca26d6429e35a73011cd36bde9c471f
Sarah Shin <sarahsujin.shin@gmail.com>
2018-09-28 00:25:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function nowServing(katzDeliLine){
   else {
     var personBeingServed = katzDeliLine[0];
     return `Currently serving ${personBeingServed}.`;
+    katzDeliLine.splice(0,1);
   }
-  katzDeliLine.splice(0,1);
 }
 
 function currentLine(katzDeliLine){
